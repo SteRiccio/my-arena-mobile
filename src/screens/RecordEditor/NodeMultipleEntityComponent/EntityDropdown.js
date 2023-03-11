17f065b37327e37dce7650d7094efea8012cb582
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-03-11 23:14:17 +0100

    entity selector: handle empty keys

--- src/screens/RecordEditor/NodeMultipleEntityComponent/EntityDropdown.js
+++ src/screens/RecordEditor/NodeMultipleEntityComponent/EntityDropdown.js
@@ -1,9 +1,13 @@
+import { Objects } from "@openforis/arena-core";
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
 
 import { Dropdown } from "../../../components";
 import { DataEntryActions } from "../../../state/dataEntry/actions";
 import { DataEntrySelectors } from "../../../state/dataEntry/selectors";
+
+const defaultIfEmpty = (defaultValue) => (text) =>
+  Objects.isEmpty(text) ? defaultValue : text;
 
 export const EntityDropdown = () => {
   const dispatch = useDispatch();
@@ -29,7 +33,7 @@ export const EntityDropdown = () => {
   const dropdownItems = entitiesUuidsAndKeyValues.map(
     ({ uuid, keyValues }) => ({
       value: uuid,
-      label: keyValues.join(" - "),
+      label: defaultIfEmpty("--- EMPTY ---")(keyValues.join(" - ")),
     })
   );
 
