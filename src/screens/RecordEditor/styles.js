24712f64d84942f03d1b5da8d07f7b8be2af808a
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-05-20 23:37:20 +0200

    record editor style

--- src/screens/RecordEditor/styles.js
+++ src/screens/RecordEditor/styles.js
@@ -1,19 +1,7 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-  titleText: {
-    flex: 1,
-  },
-  pagesNavigatorContainer: {
-    flex: 0.8,
-    backgroundColor: "#38C8EC",
-    padding: 10,
-    borderWidth: 1,
-  },
   internalContainer: {
     flex: 1,
-  },
-  closeButton: {
-    alignSelf: "flex-end",
   },
 });
