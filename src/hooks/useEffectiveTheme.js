6bd06a9174b3d621889b7500148eabcd60285eca
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-06-26 22:58:12 +0200

    code cleanup

--- src/hooks/useEffectiveTheme.js
+++ src/hooks/useEffectiveTheme.js
@@ -1,32 +1,18 @@
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Appearance } from "react-native";
+import { useColorScheme } from "react-native";
 
 import { Themes, ThemesSettings } from "model";
 import { SettingsSelectors } from "state";
 
 export const useEffectiveTheme = () => {
-  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+  const colorScheme = useColorScheme();
 
-  useEffect(() => {
-    const appearanceChangeSubscription = Appearance.addChangeListener(
-      (appearancePreferences) => {
-        setColorScheme(appearancePreferences.colorScheme);
-      }
-    );
-    return () => {
-      appearanceChangeSubscription.remove();
-    };
-  });
-
-  return useSelector((state) => {
+  const themeSetting = useSelector((state) => {
     const settings = SettingsSelectors.selectSettings(state);
-
-    const { theme: themeSetting } = settings;
-
-    if (themeSetting === ThemesSettings.systemDefault) {
-      return colorScheme === "dark" ? Themes.dark : Themes.light;
-    }
-    return themeSetting;
+    return settings.theme;
   });
+  if (themeSetting === ThemesSettings.systemDefault) {
+    return colorScheme === "dark" ? Themes.dark : Themes.light;
+  }
+  return themeSetting;
 };
