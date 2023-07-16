219bfeb8cb378db4e1a62fe6bd4342c4e2ffcaa2
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-07-16 21:18:47 +0200

    data table: code cleanup

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Banner, DataTable as RNPDataTable } from "react-native-paper";
 
 import { Arrays } from "@openforis/arena-core";
@@ -8,15 +8,12 @@ import { Checkbox } from "./Checkbox";
 import { ScrollView } from "./ScrollView";
 import { VView } from "./VView";
 
-const longPressDelayMs = 1000;
-const maxPressInLocationYDiff = 10;
-
 export const DataTable = (props) => {
   const {
     columns,
     rows,
     onRowPress: onRowPressProp,
-    onRowLongPress,
+    onRowLongPress: onRowLongPressProp,
     onSelectionChange,
     onDeleteSelectedRowIds,
     selectable,
@@ -25,27 +22,14 @@ export const DataTable = (props) => {
   const { t } = useTranslation();
 
   const [state, setState] = useState({ selectedRowIds: [] });
-  const longPressTimeoutRef = useRef(null);
-  const pressInLocationYRef = useRef(null);
 
   const { selectedRowIds, selectionEnabled } = state;
-
-  const clearLongPressTimeout = () => {
-    if (longPressTimeoutRef.current) {
-      clearTimeout(longPressTimeoutRef.current);
-      longPressTimeoutRef.current = null;
-    }
-  };
 
   useEffect(() => {
     setState((statePrev) => ({
       ...statePrev,
       selectedRowIds: [],
     }));
-
-    return () => {
-      clearLongPressTimeout();
-    };
   }, [rows]);
 
   const onRowSelect = useCallback(
@@ -71,37 +55,27 @@ export const DataTable = (props) => {
     [selectedRowIds, onDeleteSelectedRowIds]
   );
 
-  const onRowPressIn = ({ event, row }) => {
-    if (selectable) {
-      pressInLocationYRef.current = event.nativeEvent.locationY;
-      clearLongPressTimeout();
-
-      longPressTimeoutRef.current = setTimeout(() => {
-        longPressTimeoutRef.current = null;
+  const onRowPress = useCallback(
+    (row) => {
+      if (selectionEnabled) {
         onRowSelect(row);
-        onRowLongPress?.(row);
-      }, longPressDelayMs);
-    }
-  };
-
-  const onRowPressOut = ({ event, row }) => {
-    if (longPressTimeoutRef.current) {
-      // long press timeout not reached yet: only a "short" press
-      clearLongPressTimeout();
-
-      const locationYDiff = Math.abs(
-        pressInLocationYRef.current - event.nativeEvent.locationY
-      );
-      if (locationYDiff <= maxPressInLocationYDiff) {
-        if (selectionEnabled) {
-          onRowSelect(row);
-        } else {
-          onRowPressProp?.(row);
-        }
+      } else {
+        onRowPressProp?.(row);
       }
-    }
-    pressInLocationYRef.current = null;
-  };
+    },
+    [onRowPressProp, onRowSelect]
+  );
+
+  const onRowLongPress = useCallback(
+    (row) => {
+      if (selectable) {
+        onRowSelect(row);
+      } else {
+        onRowLongPressProp?.(row);
+      }
+    },
+    [onRowLongPressProp, onRowSelect]
+  );
 
   return (
     <VView style={{ flex: 1 }}>
@@ -131,13 +105,11 @@ export const DataTable = (props) => {
         </RNPDataTable.Header>
         <ScrollView persistentScrollbar>
           {rows.map((row) => (
-            <RNPDataTable.Row
-              key={row.key}
-              onPressIn={(event) => onRowPressIn({ event, row })}
-              onPressOut={(event) => onRowPressOut({ event, row })}
-            >
+            <RNPDataTable.Row key={row.key}>
               {columns.map(({ key: columnKey, style, cellRenderer = null }) => (
                 <RNPDataTable.Cell
+                  onPress={() => onRowPress(row)}
+                  onLongPress={() => onRowLongPress(row)}
                   key={columnKey}
                   style={style}
                   textStyle={{ flex: 1 }}
