4481225da1a2c9c9d90ba1b4c0ef88da81f38e50
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-10-07 22:35:08 +0200

    code preview: layout adjustments

--- src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCodeComponent/NodeCodePreview.js
+++ src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCodeComponent/NodeCodePreview.js
@@ -1,30 +1,70 @@
 import PropTypes from "prop-types";
-import { Chip } from "react-native-paper";
 
 import { NodeDefs } from "@openforis/arena-core";
 
 import { Button } from "components/Button";
 import { HView } from "components/HView";
 
+import styles from "./styles";
+
+const OpenDropdownButton = (props) => {
+  const { onPress, textKey, textParams } = props;
+
+  return (
+    <Button
+      contentStyle={{ flexDirection: "row-reverse" }}
+      icon="chevron-down"
+      mode="contained-tonal"
+      textKey={textKey}
+      textParams={textParams}
+      onPress={onPress}
+      style={styles.openDropdownButton}
+    />
+  );
+};
+
+OpenDropdownButton.propTypes = {
+  onPress: PropTypes.func.isRequired,
+  textKey: PropTypes.string,
+  testParams: PropTypes.object,
+};
+
+OpenDropdownButton.defaultProps = {
+  textKey: "dataEntry:code.selectItem",
+};
+
 export const NodeCodePreview = (props) => {
   const { itemLabelFunction, nodeDef, openEditDialog, selectedItems } = props;
 
+  const multiple = NodeDefs.isMultiple(nodeDef);
+
   return (
     <HView style={{ flexWrap: "wrap" }}>
-      {selectedItems.map((item) => (
-        <Chip
-          key={item.uuid}
+      {multiple ? (
+        <>
+          {selectedItems.map((item) => (
+            <Button
+              key={item.uuid}
+              mode="outlined"
+              onPress={openEditDialog}
+              style={styles.previewItem}
+            >
+              {itemLabelFunction(item)}
+            </Button>
+          ))}
+          <OpenDropdownButton
+            onPress={openEditDialog}
+            textParams={{ count: multiple ? 2 : 1 }}
+          />
+        </>
+      ) : (
+        <OpenDropdownButton
           onPress={openEditDialog}
-          style={{ margin: 2, padding: 4 }}
-        >
-          {itemLabelFunction(item)}
-        </Chip>
-      ))}
-      {(selectedItems.length === 0 || NodeDefs.isMultiple(nodeDef)) && (
-        <Button
-          textKey="dataEntry:code.selectItem"
-          textParams={{ count: NodeDefs.isMultiple(nodeDef) ? 2 : 1 }}
-          onPress={openEditDialog}
+          textKey={
+            selectedItems.length === 1
+              ? itemLabelFunction(selectedItems[0])
+              : "dataEntry:code.selectItem"
+          }
         />
       )}
     </HView>
--- src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCodeComponent/styles.js
+++ src/screens/RecordEditor/NodeComponentSwitch/nodeTypes/NodeCodeComponent/styles.js
@@ -9,4 +9,13 @@ export default StyleSheet.create({
     paddingHorizontal: 4,
     paddingVertical: 4,
   },
+  previewItem: {
+    margin: 2,
+    padding: 4,
+  },
+  openDropdownButton: {
+    borderRadius: 6,
+    padding: 2,
+    margin: 2,
+  },
 });
