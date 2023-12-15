08034048615535c18b3aa602d3e4029211272f7e
Stefano Ricci <SteRiccio@users.noreply.github.com>
2023-12-15 22:13:50 +0100

    code cleanup

--- src/state/deviceInfo/actions.js
+++ src/state/deviceInfo/actions.js
@@ -48,6 +48,51 @@ const initDeviceInfo = () => async (dispatch) => {
   );
 };
 
+const determineActionPaylodForBatteryChange = ({
+  batteryLevel,
+  batteryState,
+  batteryStateChanged,
+  batteryLevelMeasureStartTime,
+  batteryLevelAtStartTime,
+}) => {
+  const payload = {};
+  Object.assign(payload, {
+    batteryLevel,
+    batteryState,
+    batteryTimeToDischarge: null,
+    batteryTimeToFullCharge: null,
+  });
+  if (
+    batteryStateChanged ||
+    ![BatteryState.unplugged, BatteryState.charging].includes(batteryState)
+  ) {
+    Object.assign(payload, {
+      batteryLevelMeasureStartTime: Date.now(),
+      batteryLevelAtStartTime: batteryLevel,
+    });
+  } else {
+    const elapsedTime = Date.now() - batteryLevelMeasureStartTime;
+    const chargeDiff = batteryLevel - batteryLevelAtStartTime;
+    const batteryLevelToReach =
+      batteryState === BatteryState.unplugged ? batteryLevel : 1 - batteryLevel;
+    const chargeTimeDiff = Math.ceil(
+      (elapsedTime * batteryLevelToReach) / chargeDiff
+    );
+    if (batteryState === BatteryState.unplugged) {
+      const timeLeft = -chargeTimeDiff;
+      if (timeLeft >= 0) {
+        payload.batteryTimeToDischarge = timeLeft;
+      }
+    } else if (batteryState === BatteryState.charging) {
+      const timeLeft = chargeTimeDiff;
+      if (timeLeft >= 0) {
+        payload.batteryTimeToFullCharge = chargeTimeDiff;
+      }
+    }
+  }
+  return payload;
+};
+
 const updatePowerStateAndFreeDiskStorage = () => async (dispatch, getState) => {
   const state = getState();
   const {
@@ -62,54 +107,27 @@ const updatePowerStateAndFreeDiskStorage = () => async (dispatch, getState) => {
   const freeDiskStorage = await Files.getFreeDiskStorage();
 
   const batteryStateChanged = batteryState !== batteryStatePrev;
-  if (
-    batteryLevel === batteryLevelPrev &&
-    !batteryStateChanged &&
-    freeDiskStorage === freeDiskStoragePrev
-  ) {
+  const batteryLevelChanged = batteryLevel !== batteryLevelPrev;
+  const freeDiskStorageChanged = freeDiskStorage !== freeDiskStoragePrev;
+
+  if (!batteryLevelChanged && !batteryStateChanged && !freeDiskStorageChanged) {
     // do not update state
     return;
   }
   const payload = {};
-  if (batteryLevel !== batteryLevelPrev || batteryStateChanged) {
-    Object.assign(payload, {
-      batteryLevel,
-      batteryState,
-      batteryTimeToDischarge: null,
-      batteryTimeToFullCharge: null,
-    });
-    if (
-      batteryStateChanged ||
-      ![BatteryState.unplugged, BatteryState.charging].includes(batteryState)
-    ) {
-      Object.assign(payload, {
-        batteryLevelMeasureStartTime: Date.now(),
-        batteryLevelAtStartTime: batteryLevel,
-      });
-    } else {
-      const elapsedTime = Date.now() - batteryLevelMeasureStartTime;
-      const chargeDiff = batteryLevel - batteryLevelAtStartTime;
-      const batteryLevelToReach =
-        batteryState === BatteryState.unplugged
-          ? batteryLevel
-          : 1 - batteryLevel;
-      const chargeTimeDiff = Math.ceil(
-        (elapsedTime * batteryLevelToReach) / chargeDiff
-      );
-      if (batteryState === BatteryState.unplugged) {
-        const timeLeft = -chargeTimeDiff;
-        if (timeLeft >= 0) {
-          payload.batteryTimeToDischarge = timeLeft;
-        }
-      } else if (batteryState === BatteryState.charging) {
-        const timeLeft = chargeTimeDiff;
-        if (timeLeft >= 0) {
-          payload.batteryTimeToFullCharge = chargeTimeDiff;
-        }
-      }
-    }
+  if (batteryLevelChanged || batteryStateChanged) {
+    Object.assign(
+      payload,
+      determineActionPaylodForBatteryChange({
+        batteryLevel,
+        batteryState,
+        batteryStateChanged,
+        batteryLevelMeasureStartTime,
+        batteryLevelAtStartTime,
+      })
+    );
   }
-  if (freeDiskStorage !== freeDiskStoragePrev) {
+  if (freeDiskStorageChanged) {
     Object.assign(payload, { freeDiskStorage });
   }
   dispatch({ type: DEVICE_POWER_STATE_SET, payload });
