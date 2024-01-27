bfe60f59899dc96ef7cf8b876320e825d8923971
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-01-27 15:00:38 +0100

    update yarn to v4 (WIP)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,19 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    presets: [
+      "babel-preset-expo",
+      ["module:@react-native/babel-preset"],
+      [
+        "module:metro-react-native-babel-preset",
+        { useTransformReactJSXExperimental: true },
+      ],
+      "@babel/preset-react",
+      "@babel/preset-typescript",
+    ],
     plugins: [
+      ["@babel/plugin-transform-react-jsx", { runtime: "automatic" }],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
       [
         "module-resolver",
         {
@@ -10,7 +21,6 @@ module.exports = function (api) {
         },
       ],
       "react-native-reanimated/plugin",
-      "transform-remove-strict-mode",
     ],
     env: {
       production: {
