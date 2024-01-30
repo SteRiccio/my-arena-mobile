d011cb954d13e80bb2a8721e7ab9f5bc198f39a1
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-01-30 22:40:03 +0100

    code cleanup

--- src/service/index.js
+++ src/service/index.js
@@ -1,5 +1,6 @@
 export { AuthService } from "./authService";
 export { PreferencesService } from "./preferencesService";
+export { RecordFileService } from "./recordFileService";
 export { RecordService } from "./recordService";
 export { SettingsService } from "./settingsService";
 export { SurveyService } from "./surveyService";
