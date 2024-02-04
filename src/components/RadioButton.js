a1d7c6d245aabef24d011840da4a361f3a9aa95a
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-02-04 22:14:27 +0100

    fixed DeepScan issue

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -2,11 +2,10 @@ import { RadioButton as RNPRadioButton } from "react-native-paper";
 import PropTypes from "prop-types";
 
 export const RadioButton = (props) => {
-  const { checked, disabled, key, label, onPress, style, value } = props;
+  const { checked, disabled, label, onPress, style, value } = props;
 
   return (
     <RNPRadioButton.Item
-      key={key}
       disabled={disabled}
       label={label}
       mode="android"
@@ -21,7 +20,6 @@ export const RadioButton = (props) => {
 RadioButton.propTypes = {
   checked: PropTypes.bool,
   disabled: PropTypes.bool,
-  key: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   label: PropTypes.string,
   onPress: PropTypes.func.isRequired,
   style: PropTypes.object,
