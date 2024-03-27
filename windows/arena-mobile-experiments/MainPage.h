ab8f5b45eb1f76dde349d56eb3c5c872d22c0aa7
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-03-27 23:01:08 +0100

    updated dependencies

---
+++ windows/arena-mobile-experiments/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::arena-mobile-experiments::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::arena-mobile-experiments::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
