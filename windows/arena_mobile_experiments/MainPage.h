3a116e725e3d56d121e556196e62550586a85a3a
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-04-01 23:33:18 +0200

    added react-native-windows

---
+++ windows/arena_mobile_experiments/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::arena_mobile_experiments::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::arena_mobile_experiments::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
