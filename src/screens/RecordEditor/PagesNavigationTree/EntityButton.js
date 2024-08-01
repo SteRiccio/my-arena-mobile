01ebe60e5933656357f6e6b676dd9ba629ce63fc
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-08-02 00:32:57 +0200

    pages tree: fixed press issue

--- src/screens/RecordEditor/PagesNavigationTree/EntityButton.js
+++ src/screens/RecordEditor/PagesNavigationTree/EntityButton.js
@@ -1,28 +1,15 @@
-import { useCallback } from "react";
-import { TouchableOpacity } from "react-native";
-import { useDispatch } from "react-redux";
-
 import { Text } from "components";
-import { DataEntryActions } from "state";
 
 export const EntityButton = ({ treeNode, isCurrentEntity }) => {
-  const { label, entityPointer } = treeNode;
-
-  const dispatch = useDispatch();
-
-  const onPress = useCallback(() => {
-    dispatch(DataEntryActions.selectCurrentPageEntity(entityPointer));
-  }, [entityPointer]);
+  const { label } = treeNode;
 
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text
-        style={{
-          fontSize: isCurrentEntity ? 24 : 20,
-          fontWeight: isCurrentEntity ? "bold" : "normal",
-        }}
-        textKey={label}
-      />
-    </TouchableOpacity>
+    <Text
+      style={{
+        fontSize: isCurrentEntity ? 24 : 20,
+        fontWeight: isCurrentEntity ? "bold" : "normal",
+      }}
+      textKey={label}
+    />
   );
 };
