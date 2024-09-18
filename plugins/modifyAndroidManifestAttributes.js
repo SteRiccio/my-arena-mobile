626b25dcda23de9db8afeba91ce5ab133d22b9b9
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-09-18 22:52:39 +0200

    code cleanup

--- plugins/modifyAndroidManifestAttributes.js
+++ plugins/modifyAndroidManifestAttributes.js
@@ -3,21 +3,16 @@ const { withAndroidManifest } = require("@expo/config-plugins");
 module.exports = function androidManifestPlugin(config, data) {
   return withAndroidManifest(config, async (config) => {
     const androidManifest = config.modResults.manifest;
-    const categories = Object.keys(data);
-
-    categories.forEach((category) => {
-      if (
-        androidManifest[category] &&
-        Array.isArray(androidManifest[category]) &&
-        androidManifest[category].length > 0
-      ) {
-        const attributes = Object.keys(data[category]);
-        attributes.forEach((attribute) => {
-          androidManifest[category][0].$[attribute] = data[category][attribute];
-        });
+    Object.entries(data || {}).forEach(([category, categoryData]) => {
+      const manifestCategoryItems = androidManifest[category] || [];
+      if (manifestCategoryItems.length > 0) {
+        Object.entries(categoryData || {}).forEach(
+          ([attribute, attributeValue]) => {
+            manifestCategoryItems[0].$[attribute] = attributeValue;
+          }
+        );
       }
     });
-
     return config;
   });
 };
