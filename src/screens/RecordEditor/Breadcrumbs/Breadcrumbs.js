1af787a1a39c3d3c070873e44a1592b0656285c0
Stefano Ricci <SteRiccio@users.noreply.github.com>
2024-09-28 15:54:41 +0200

    breadcrumbs code cleanup

--- src/screens/RecordEditor/Breadcrumbs/Breadcrumbs.js
+++ src/screens/RecordEditor/Breadcrumbs/Breadcrumbs.js
@@ -1,15 +1,12 @@
 import React, { useCallback, useEffect, useRef } from "react";
 import { ScrollView } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-import { NodeDefs, Objects, Records, Surveys } from "@openforis/arena-core";
-
-import { RecordNodes } from "model/utils/RecordNodes";
-import { SurveySelectors } from "../../../state/survey/selectors";
 import { DataEntrySelectors } from "../../../state/dataEntry/selectors";
 import { HView } from "../../../components";
 import { DataEntryActions } from "../../../state/dataEntry/actions";
 import { BreadcrumbItem } from "./BreadcrumbItem";
+import { useBreadcrumbItems } from "./useBreadcrumbItems";
 
 import styles from "./styles";
 
@@ -20,94 +17,20 @@ export const Breadcrumbs = () => {
   const dispatch = useDispatch();
   const scrollViewRef = useRef(null);
 
-  const survey = SurveySelectors.useCurrentSurvey();
-  const lang = SurveySelectors.useCurrentSurveyPreferredLang();
   const currentPageEntity = DataEntrySelectors.useCurrentPageEntity();
-  const { entityUuid, parentEntityUuid, entityDef } = currentPageEntity;
-  const actualEntityUuid = entityUuid ?? parentEntityUuid;
+  const { entityDef } = currentPageEntity;
   const entityDefUuid = entityDef.uuid;
-
-  const itemLabelFunction = useCallback(
-    ({ nodeDef, record = null, entity = null, parentEntity = null }) => {
-      const nodeDefLabel = NodeDefs.getLabelOrName(nodeDef, lang);
-
-      if (
-        NodeDefs.isRoot(nodeDef) ||
-        (NodeDefs.isMultiple(nodeDef) && parentEntity)
-      ) {
-        const keyValuesByName =
-          RecordNodes.getEntitySummaryValuesByNameFormatted({
-            survey,
-            record,
-            entity,
-            lang,
-          });
-        return nodeDefLabel + `[${Object.values(keyValuesByName)}]`;
-      }
-      return nodeDefLabel;
-    },
-    [lang, survey]
-  );
 
   useEffect(() => {
     // scroll to the end (right) when selected entity changes
     scrollViewRef?.current?.scrollToEnd({ animated: true });
   }, [entityDefUuid]);
 
-  const items = useSelector((state) => {
-    if (!actualEntityUuid) return [];
-
-    const record = DataEntrySelectors.selectRecord(state);
-    const survey = SurveySelectors.selectCurrentSurvey(state);
-
-    const _items = [];
-
-    if (parentEntityUuid && !entityUuid) {
-      _items.push({
-        parentEntityUuid,
-        entityDefUuid,
-        entityUuid: null,
-        name: itemLabelFunction({ nodeDef: entityDef }),
-      });
-    }
-
-    let currentEntity = Records.getNodeByUuid(actualEntityUuid)(record);
-
-    while (currentEntity) {
-      const parentEntity = Records.getParent(currentEntity)(record);
-
-      const currentEntityDef = Surveys.getNodeDefByUuid({
-        survey,
-        uuid: currentEntity.nodeDefUuid,
-      });
-      const itemName = itemLabelFunction({
-        nodeDef: currentEntityDef,
-        record,
-        parentEntity,
-        entity: currentEntity,
-      });
-
-      _items.unshift({
-        parentEntityUuid: parentEntity?.uuid,
-        entityDefUuid: currentEntityDef.uuid,
-        entityUuid: currentEntity.uuid,
-        name: itemName,
-      });
-
-      currentEntity = parentEntity;
-    }
-    return _items;
-  }, Objects.isEqual);
+  const items = useBreadcrumbItems();
 
   const onItemPress = useCallback(
-    ({ parentEntityUuid, entityDefUuid, entityUuid }) => {
-      dispatch(
-        DataEntryActions.selectCurrentPageEntity({
-          parentEntityUuid,
-          entityDefUuid,
-          entityUuid,
-        })
-      );
+    (pageEntityItem) => {
+      dispatch(DataEntryActions.selectCurrentPageEntity(pageEntityItem));
     },
     [dispatch]
   );
