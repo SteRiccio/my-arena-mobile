29b1e516e1d83ff3f1de36eff88671a5252ca483
Stefano Ricci <1219739+SteRiccio@users.noreply.github.com>
2025-02-13 21:42:43 +0100

    Deprecation message (#124)
    * show deprecation message at startup
    
    * layout adjustments
    
    * code cleanup
    
    ---------
    
    Co-authored-by: Stefano Ricci <SteRiccio@users.noreply.github.com>

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 
 import { AppLogo } from "appComponents/AppLogo";
@@ -6,16 +7,32 @@ import { GpsLockingEnabledWarning } from "appComponents/GpsLockingEnabledWarning
 import { LoginInfo } from "appComponents/LoginInfo";
 import { VersionNumberInfoButton } from "appComponents/VersionNumberInfoButton";
 import { Button, ScreenView, Text, VView } from "components";
-import { SurveySelectors } from "state";
+import { MessageActions, SurveySelectors } from "state";
 
 import { screenKeys } from "../screenKeys";
 import { SelectedSurveyContainer } from "./SelectedSurveyContainer";
 
 import styles from "./styles";
+import { Environment } from "utils/Environment";
 
 export const HomeScreen = () => {
+  const dispatch = useDispatch();
   const navigation = useNavigation();
   const survey = SurveySelectors.useCurrentSurvey();
+
+  useEffect(() => {
+    const link =
+      Environment.isAndroid || Environment.isExpoGo
+        ? "https://play.google.com/store/apps/details?id=org.openforis.arena_mobile"
+        : "https://apps.apple.com/app/open-foris-arena-mobile-2/id6741569376";
+    dispatch(
+      MessageActions.setMessage({
+        content: "app:deprecationMessage.content",
+        details: "app:deprecationMessage.details",
+        detailsParams: { link },
+      })
+    );
+  }, [dispatch]);
 
   const surveySelected = !!survey;
 
